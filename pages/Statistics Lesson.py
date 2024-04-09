3d00b2dc354a05acfcfc2ae1af9a22cf48f67aa8
calwinship <winship.cal@gmail.com>
2024-04-09 22:34:58 +0100

    new lesson template

--- pages/Statistics Lesson.py
+++ pages/Statistics Lesson.py
@@ -57,7 +57,7 @@ prompt_template = st.session_state[id]
 with st.expander('Concept 1: Sampling Variability'):
     st.write("The objective is: ")
     st.success(f"{objectives}", icon="✅")
-    st.info()
+    # st.info()
     # st.write("This is a self-directed lesson meaning you need to extract the information to answer the question and understand the topic. Take notes!")
         # Display chat messages from history on app rerun
     for message in prompt_template:
