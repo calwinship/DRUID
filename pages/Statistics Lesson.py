a093b7a908e338a64bd261f595ec67120fa33e11
calwinship <winship.cal@gmail.com>
2024-04-09 23:07:36 +0100

    new lesson template

--- pages/Statistics Lesson.py
+++ pages/Statistics Lesson.py
@@ -25,6 +25,9 @@ with st.expander('Links'):
     st.info(link2)
     st.info(link3)
 
+with st.expander("Q1"):
+    st.image("DRUID/images/Exam17_2_Q8a.png")
+
 
 # config items
 name = CONFIG["name"]
